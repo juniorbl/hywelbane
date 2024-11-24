6cd7048dfe03c59d57c18aa6c9b2a53549c5c4a7
Carlos Luz <carlosluzjunior@gmail.com>
2024-11-24 12:04:57 -0500

    add inverse mass logic

--- src/physics/particle.go
+++ src/physics/particle.go
@@ -2,18 +2,26 @@ package physics
 
 type Particle struct {
 	Position, Velocity, Acceleration, Forces Vec2
-	Mass                                     float64
+	Mass, InverseMass                        float64
 }
 
 func NewParticle(x float64, y float64, mass float64) *Particle {
+	var inverseMass float64
+	if mass == 0.0 {
+		inverseMass = 0.0
+	} else {
+		inverseMass = 1.0 / mass
+	}
+
 	return &Particle{
-		Position: *NewVec2(x, y),
-		Mass:     mass,
+		Position:    *NewVec2(x, y),
+		Mass:        mass,
+		InverseMass: inverseMass,
 	}
 }
 
 func (p *Particle) Integrate(deltaInSecs float64) {
-	p.Acceleration = *p.Forces.Divide(p.Mass)
+	p.Acceleration = *p.Forces.Multiply(p.InverseMass) // or use the mass: p.Acceleration = *p.Forces.Divide(p.Mass)
 
 	// Implicit Euler integration
 	p.Velocity.Add(p.Acceleration.Multiply(deltaInSecs))
